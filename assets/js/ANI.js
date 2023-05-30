090f5b9db5062a27186a229bc77cfe035cb43fe1
yz-salas <115598357+yz-salas@users.noreply.github.com>
2023-05-29 23:44:50 -0400

    portafolio

---
+++ assets/js/ANI.js
@@ -0,0 +1,115 @@
+/*
+ * animaciones inicio
+ */
+
+window.sr = ScrollReveal();
+sr.reveal("#cont-link", {
+  duration: 2000,
+  origin: "right",
+  distance: "-100px",
+});
+
+sr.reveal("#cont-prec", {
+  duration: 2000,
+  origin: "top",
+  distance: "-100px",
+});
+
+/*
+ * animaciones inicio
+ */
+
+/*
+ ! animaciones abaut
+*/
+
+sr.reveal("#cont-img", {
+  duration: 3000,
+  origin: "raght",
+  distance: "-100px",
+});
+
+sr.reveal("#cont-text-img", {
+  duration: 3000,
+  origin: "left",
+  distance: "-100px",
+});
+
+sr.reveal("#cont-text", {
+  duration: 3000,
+  origin: "right",
+  distance: "-100px",
+});
+
+sr.reveal("#hello", {
+  duration: 3000,
+  origin: "left",
+  distance: "-100px",
+});
+
+sr.reveal("#regreso", {
+  duration: 2000,
+  origin: "left",
+  distance: "-200px",
+});
+
+/*
+ ! animaciones abaut
+*/
+
+/*
+ ? animaciones proyecto
+*/
+sr.reveal("#p", {
+    duration: 2000,
+    origin: "right",
+    distance: "-200px",
+})
+
+sr.reveal("#p", {
+    duration: 2000,
+    origin: "right",
+    distance: "-200px",
+})
+/*
+ ? animaciones proyecto
+*/
+
+/*
+  TODO: animaciones proyecto
+*/
+sr.reveal("#cont-int", {
+    duration: 2000,
+    origin: "right",
+    distance: "-300px",
+})
+
+sr.reveal("#cont", {
+    duration: 2000,
+    origin: "left",
+    distance: "-300px",
+})
+
+sr.reveal("#contn-text", {
+    duration: 2000,
+    origin: "right",
+    distance: "-300px",
+})
+
+sr.reveal("#cont-redes", {
+    duration: 2000,
+    origin: "top",
+    distance: "-300px",
+})
+
+sr.reveal("#icono-redes", {
+    duration: 2000,
+    origin: "top",
+    distance: "-300px",
+})
+
+
+
+/*
+  TODO: animaciones proyecto
+*/
